1e65c843589c8fef1efc0a4125cdb26cc1bf9c2a
Yusupsup9510 <138879211+Yusupsup9510@users.noreply.github.com>
2024-06-09 17:27:48 +0700

    index.php

--- index.php
+++ index.php
@@ -133,16 +133,3 @@ $img = 'https://th.bing.com/th/id/R.53e479f1a86be5db40bed48b18adb05e?rik=ADqPWiX
 				<script type="text/javascript" src="assets/js/scripts.js" defer></script>
 			</body>
 			</html>
-<!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4815965,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4815965&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->
-			</html>
